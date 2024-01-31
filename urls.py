38d336969408edf0fa45c476005ca90946b19888
user <142376370+AnuragMs1998@users.noreply.github.com>
2024-01-31 11:57:59 +0530

    Add files via upload

---
+++ urls.py
@@ -0,0 +1,16 @@
+
+from django.urls import path
+from .import views
+from med_store_api import views
+
+
+urlpatterns = [
+    
+    path('available_med', views.available_med, name='available_med_api'),
+    path('search_med', views.search_med, name='search_med'),
+    path('<int:pk>/medicine_details', views.medicine_details.as_view()),
+    path('add_medicine', views.add_medicine.as_view()),
+    path('login', views.login, name='login_api'),
+    path('signup',views.signup,name='signup_api')
+ ]
+
